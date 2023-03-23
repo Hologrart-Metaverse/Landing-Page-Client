e2a95c2022fc0a53e541ecc6b1c85917899c237f
DogukanTopcu <dogukan_topcu3575@icloud.com>
2023-03-24 01:05:16 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="h-screen w-screen max-w-screen max-h-screen min-w-screen min-h-screen overflow-hidden">
+      <h1>Landing Page</h1>
+    </div>
+  );
+}
+
+export default App;
